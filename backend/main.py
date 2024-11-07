ee9e1a6032456dbd1f68cdc6ba20cbb11dfb1343
08boramae <mail@0jae0.dev>
2024-11-07 11:01:25 +0900

    two

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
 from pydantic import BaseModel
+import json
 import sqlite3
 
 conn = sqlite3.connect('./database', check_same_thread=False)
@@ -19,20 +21,38 @@ def new_message_register(message: Message):
     cur.execute('INSERT INTO messages (name, message) VALUES (?, ?)', (message.name, message.message))
     conn.commit()
 
-def check_new_message():
+def check_message_count():
     cur = conn.cursor()
     cur.execute('SELECT COUNT(*) FROM messages')
     count = cur.fetchone()
     return count
 
-@app.get('/check_message')
-async def check_message():
-    count = check_new_message()
+def check_message_content():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM messages')
+    messages = cur.fetchall()
+    messages_list = [{'id': msg[2], 'name': msg[0], 'message': msg[1]} for msg in messages]
+    json_data = json.dumps(messages_list, ensure_ascii=False)
+    print(json_data)
+    return json_data
+
+@app.get('/check_message_count')
+async def check_message_new():
+    count = check_message_count()
     return {"count": count}
 
+@app.get('/check_messages')
+async def check_messages():
+    messages = check_message_content()
+    # parsing messages to json
+    return {"messages": messages}
+
 @app.post('/post_message')
-async def post_message(message: Message):
-    new_message_register(message)
-    return("Maybe good")
+async def post_message(message: Annotated[Message, Form()]):
+    try:
+        new_message_register(message)
+        return {"status": "200", "message": "Message posted successfully"}
+    except:
+        return {"status": "500", "message": "Internal Server Error"}
 
 init_db()
